69185e7f0955b863ff219cd024a75300d08d5413
allyyaanr <allyyanrazizah@gmail.com>
2024-03-21 15:32:42 +0100

    Mini-Project 2 WEB

---
+++ script.js
@@ -0,0 +1,61 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+const form = document.querySelector('#contactForm')
+
+form.addEventListener('submit',function(){
+    const name = document.getElementById('name')
+    const email = document.getElementById('email')
+    const message = document.getElementById('message')
+    
+    const nameValue = name.value
+    const emailValue = email.value
+    const messageValue = message.value
+
+    localStorage('name',nameValue)
+    localStorage('email',emailValue)
+    localStorage('message',messageValue)
+
+    name.value = ''
+    email.value = ''
+    message.value = ''
+
+    
+})
